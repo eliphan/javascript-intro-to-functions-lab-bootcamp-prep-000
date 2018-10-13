49a7ffb909bad2d40b3098f822a23f83146499fc
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-13 23:26:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-var uppercase = "HELLO!"
+var uppercase = 'HELLO!'
 uppercase.toUpperCase() === uppercase
 var lowercase = 'hello!'
 lowercase.toLowerCase() === lowercase
