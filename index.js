41a4b38f5c078b45065e3b4fee98e72906b5e7f8
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-13 22:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ var mixedCase = "Hi there"
 mixedCase.toLowerCase() === mixedCase
 mixedCase.toUpperCase() === mixedCase
 var hearingTest = function sayHiToGrandma(string) {
-  if string.to
+  if string
