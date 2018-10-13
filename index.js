1d6171c6afccd5455e63b9a8e7bd7c26bf1bf1d8
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-13 21:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logshout(string) {
-  console.log(string).toUpperCase()
+  console.log(string)
+  return string.toUpperCase()
 }
