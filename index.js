7bd3b4c6ce4c61c24609e41e058b526aaf9b8fb8
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-13 23:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,25 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
 function sayHiToGrandma(string) {
  if (string.toLowerCase()) {
    return "I can\'t hear you!";
  } else if (string.toUpperCase()) {
   return "YES INDEED";
- } else (string.)
-
+ } else (mixedCase) {
+   return "I love you,too";
+ }
+}
