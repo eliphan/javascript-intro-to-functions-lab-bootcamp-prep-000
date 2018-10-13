a34dd6a065e33060ec4b4aec9f07797f5b7c68cd
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-13 22:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-var uppercase = "HELLO"
-uppercase.toUpperCase() === uppercase
-var lowercase = "hello"
-lowercase.toLowerCase() === lowercase
-var mixedCase = "Hi there"
-mixedCase.toLowerCase() === mixedCase
-mixedCase.toUpperCase() === mixedCase
 var hearingTest = function sayHiToGrandma(string) {
   if (hearingTest = lowercase) {
     return "I can\'t hear you!";
